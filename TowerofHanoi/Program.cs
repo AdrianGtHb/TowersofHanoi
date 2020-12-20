0d66f1d6c0674b1a7524c2d2040ea68e0b853b42
AdrianRendl <adrian.rendl@siemens.com>
2020-12-20 20:44:56 +0100

    Projektdateien hinzufügen.

---
+++ TowerofHanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TowerofHanoi
+{
+    class Program
+    {
+        static void hanoi(int n, int start, int end)
+        {
+            if(n == 1)
+            {
+                Console.WriteLine("From " + start + " to " + end);
+            }
+            else
+            {
+                int other = 6 - (start + end);
+                hanoi(n - 1, start, other);
+                Console.WriteLine("From " + start + " to " + end);
+                hanoi(n - 1, other, end);
+            }
+        }
+        static void Main(string[] args)
+        {
+            hanoi(3, 1, 3);
+        }
+    }
+}
